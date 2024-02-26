999e86d1f12fa8932c72a016679243c65cf2029f
Artem <artem.andreyev212@gmail.com>
2024-02-26 20:40:42 +0200

    Added some functions in script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    const tabs = document.querySelectorAll(".tabheader__item");
+    const tabsContent = document.querySelectorAll(".tabcontent");
+    const tabsParent = document.querySelector(".tabheader__items");
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = "none";
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
+});
