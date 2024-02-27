f7d7d8b2108d130359b3478690691fa633f22b78
Artem <artem.andreyev212@gmail.com>
2024-02-27 21:26:47 +0200

    Added tabs functionality and animation for them

--- js/script.js
+++ js/script.js
@@ -6,11 +6,34 @@ window.addEventListener("DOMContentLoaded", () => {
 
     function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = "none";
+            item.classList.add("hide");
+            item.classList.remove("show", "fade");
         });
 
         tabs.forEach(item => {
             item.classList.remove("tabheader__item_active");
         });
     }
+
+    function showTabContent(i = 0) {  // i = 0 все равно что showTabContent(0);
+        tabsContent[i].classList.add("show", "fade");
+        tabsContent[i].classList.remove("hide");
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener("click", (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains("tabheader__item")) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i); // Проработать чтобы не осталось вопросов
+                }
+            });
+        }
+    });
 });
