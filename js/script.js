273a4a2c386c51eab00fc2080f044e882c2cb891
Artem <artem.andreyev212@gmail.com>
2024-05-20 22:14:53 +0300

    Added scripts for slider functionality, changed html to get all slider pages

--- js/script.js
+++ js/script.js
@@ -210,7 +210,7 @@ window.addEventListener("DOMContentLoaded", () => {
     //         });
     //     });
 
-    axios.get("http://localhost:3000/menu") // axios возвращает более подробные ответ
+    axios.get("http://localhost:3000/menu") // axios возвращает более подробный ответ
         .then(data => {
             data.data.forEach(({img, altimg, title, descr, price}) => { // Обращаемся именно к тем данным которые получилии, а не к общему обьекту (data.data)
                 new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
@@ -393,4 +393,55 @@ window.addEventListener("DOMContentLoaded", () => {
         .then(data => data.json())
         .then(res => console.log(res));
 
+    // Slider
+
+    // 1 вариант
+
+    const slides = document.querySelectorAll(".offer__slide");
+    const prev = document.querySelector(".offer__slider-prev");
+    const next = document.querySelector(".offer__slider-next");
+    const total = document.querySelector("#total");
+    const current = document.querySelector("#current");
+
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.next}`;
+    } else {
+        total.textContent = slides.next;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = "none");
+
+        slides[slideIndex - 1].style.display = "block";
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+    
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener("click", () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener("click", () => {
+        plusSlides(1);
+    });
 });
