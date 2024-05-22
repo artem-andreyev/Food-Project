5841d55999404dccb53646eed09736f48acb8c7b
Artem <artem.andreyev212@gmail.com>
2024-05-22 22:14:15 +0300

    Added better second slider variant wiith transition between slides (completed)

--- js/script.js
+++ js/script.js
@@ -408,9 +408,9 @@ window.addEventListener("DOMContentLoaded", () => {
     // showSlides(slideIndex);
 
     // if (slides.length < 10) {
-    //     total.textContent = `0${slides.next}`;
+    //     total.textContent = `0${slides.length}`;
     // } else {
-    //     total.textContent = slides.next;
+    //     total.textContent = slides.length;
     // }
 
     // function showSlides(n) {
@@ -447,16 +447,25 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // 2 вариант
 
-    const slides = document.querySelectorAll(".offer__slide");
-    const prev = document.querySelector(".offer__slider-prev");
-    const next = document.querySelector(".offer__slider-next");
-    const total = document.querySelector("#total");
-    const current = document.querySelector("#current");
-    const slidesWrapper = document.querySelector(".offer__slider-wraper");
-    const slidesField = document.querySelector(".offer__slider-inner");
-    const width = window.getComputedStyle(slidesWrapper).width;
+    const slides = document.querySelectorAll(".offer__slide"),
+          prev = document.querySelector(".offer__slider-prev"),
+          next = document.querySelector(".offer__slider-next"),
+          total = document.querySelector("#total"),
+          current = document.querySelector("#current"),
+          slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+          slidesField = document.querySelector(".offer__slider-inner"),
+          width = window.getComputedStyle(slidesWrapper).width;
 
     let slideIndex = 1;
+    let offset = 0;
+
+    if (slides.length < 10) {
+        total.textContent = `0${slide.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slide.length;
+        current.textContent = slideIndex;
+    }
 
     slidesField.style.width = 100 * slides.length + "%";
     slidesField.style.display = "flex";
@@ -466,6 +475,50 @@ window.addEventListener("DOMContentLoaded", () => {
 
     slides.forEach(slide => {
         slide.style.width = width;
+    });
+
+    next.addEventListener("click", () => {
+        if (offset == +width.slice(0, width.length -2) * (slides.length -1)) { // width = "500px"; slice - убрали px
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length -2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+    prev.addEventListener("click", () => {
+        if (offset = 0) {
+            offset = +width.slice(0, width.length -2) * (slides.length -1)
+        } else {
+            offset -= +width.slice(0, width.length -2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
     });
 
 });
